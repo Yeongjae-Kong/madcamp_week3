53018730f17f52cca0eb1a22bee94f54c8445983
kgy1008 <gayeon1008@g.skku.edu>
2024-01-13 17:11:31 +0900

    react cam

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
-import logo from './logo.svg';
+import React, {useRef} from 'react';
 import './App.css';
+import Webcam from 'react-webcam';
+import * as poseDetection from '@tensorflow-models/pose-detection';
+import * as tf from '@tensorflow/tfjs-core';
+// Register one of the TF.js backends.
+import '@tensorflow/tfjs-backend-webgl';
+// import '@tensorflow/tfjs-backend-wasm';
+
 
 function App() {
+  const webcamRef = useRef(null); 
+  const canvasRef = useRef(null);
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <header className="App-header">
+          <Webcam
+            ref={webcamRef}
+            style = {{
+              position: "absolute",
+              marginLeft: "auto",
+              marginRight: "auto",
+              left: 0,
+              right: 0,
+              textAlign: "center",
+              zindex : 9,
+              width: 640,
+              height: 480
+            }}
+            />
+
+            <canvas 
+              ref={canvasRef}
+              style = {{
+                position: "absolute",
+                marginLeft: "auto",
+                marginRight: "auto",
+                left: 0,
+                right: 0,
+                textAlign: "center",
+                zindex : 9,
+                width: 640,
+                height: 480
+              }} />
+        </header>
     </div>
   );
 }
